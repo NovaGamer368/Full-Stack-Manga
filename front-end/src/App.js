7a7dd5d88f746271a89929c4bf06f99d722061d6
Brennan Phifer <bman5803@gmail.com>
2024-10-15 15:12:59 -0600

    Super basic react app finish

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./pages/AboutPage";
 import MangaListPage from "./pages/MangaListPage";
 import MangaPage from "./pages/MangaPage";
 import NavBar from "./components/NavBar";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/mangas" element={<MangaListPage />} />
             <Route path="/mangas/:mangaId" element={<MangaPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
       </div>
--- front-end/src/pages/MangaPage.js
+++ front-end/src/pages/MangaPage.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import mangas from "../components/manga-content";
+import NotFoundPage from "./NotFoundPage";
 
 const MangaPage = () => {
   const { mangaId } = useParams();
 
   const manga = mangas.find((manga) => manga.id === mangaId);
+
+  if (!manga) {
+    return <NotFoundPage />;
+  }
+
   return (
     <>
       <h1>MangaPage wth the ID of {mangaId}</h1>
