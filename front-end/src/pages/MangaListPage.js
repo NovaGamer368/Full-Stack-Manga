5132dd76e38f9e26eb83cc2982d8eabb12bff336
Brennan Phifer <bman5803@gmail.com>
2024-10-15 14:50:22 -0600

    Pages with router implemented

---
+++ front-end/src/pages/MangaListPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MangaListPage = () => {
+  return <h1>MangaListPage</h1>;
+};
+
+export default MangaListPage;
---
+++ front-end/src/pages/MangaPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MangaPage = () => {
+  return <h1>MangaPage</h1>;
+};
+
+export default MangaPage;
