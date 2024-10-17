4c075444137c1b26501c5cdaac9b28913e32dbe0
Brennan Phifer <bman5803@gmail.com>
2024-10-17 14:00:47 -0600

    Styling change and list logic

--- front-end/src/components/MangasList.js
+++ front-end/src/components/MangasList.js
@@ -4,16 +4,30 @@ import { Link } from "react-router-dom";
 const MangasList = ({ mangas }) => {
   return (
     <>
-      {mangas?.map((manga) => (
-        <Link
-          key={manga.id}
-          className="manga-list-item"
-          to={`/mangas/${manga.id}`}
-        >
-          <h3>{manga.title}</h3>
-          <p>{manga.content[0].substring(0, 150)}...</p>
-        </Link>
-      ))}
+      {mangas && (
+        <>
+          {mangas.map((manga) => (
+            <Link
+              key={manga._id}
+              className="manga-list-item"
+              to={`/mangas/${manga._id}`}
+            >
+              <div>
+                <img
+                  src={manga.imageUrl}
+                  alt={manga.title + " manga cover"}
+                  height={200}
+                  width={143}
+                />
+                <section>
+                  <h3>{manga.title}</h3>
+                  <p>{manga.description[0].substring(0, 150)}...</p>
+                </section>
+              </div>
+            </Link>
+          ))}
+        </>
+      )}
     </>
   );
 };
--- front-end/src/pages/MangaListPage.js
+++ front-end/src/pages/MangaListPage.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import mangas from "../components/manga-content";
 import MangasList from "../components/MangasList";
+import axios from "axios";
 
 const MangaListPage = () => {
+  const [mangaList, setMangaList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchManga = async () => {
+      const response = await axios.get("/api/mangas");
+      const data = response.data;
+      console.log(data);
+      setMangaList(data);
+      setIsLoading(false);
+    };
+    fetchManga();
+  }, []);
+  if (isLoading) {
+    return <h1>LOADING...</h1>;
+  }
   return (
     <>
       <h1>Manga List</h1>
-      <MangasList mangas={mangas} />
+      <MangasList mangas={mangaList} />
     </>
   );
 };
